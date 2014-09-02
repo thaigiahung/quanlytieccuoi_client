5584ecb82e9bd955abb82c52454cbb320c7aa2a8
Gia Hung Thai <hung.tg89@gmail.com>
2014-09-02 16:49:26 +0700

    Đã load được dữ liệu cho GridView nhưng chưa có event click

--- src/com/dm/zbar/android/examples/MainActivity.java
+++ src/com/dm/zbar/android/examples/MainActivity.java
@@ -12,15 +12,26 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.TextView;
 import android.widget.Toast;
 import com.dm.zbar.android.scanner.ZBarConstants;
 import com.dm.zbar.android.scanner.ZBarScannerActivity;
@@ -31,6 +42,7 @@ public class MainActivity extends Activity {
     private static final int ZBAR_SCANNER_REQUEST = 0;
     private static final int ZBAR_QR_SCANNER_REQUEST = 1;
     private static Activity mAct;
+    public JSONArray jArr;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -62,6 +74,18 @@ public class MainActivity extends Activity {
         
         String url = "http://192.168.1.53/quanlytieccuoi_server/data.php";
         postData(url);
+        
+        GridView gv = (GridView)findViewById(R.id.gridView1);
+        gv.setAdapter(new myadapter(this));
+        gv.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				// TODO Auto-generated method stub
+				Toast.makeText(getApplicationContext(), position+"", Toast.LENGTH_SHORT).show();
+			}
+		});
     }
     
     //http://www.androidsnippets.com/executing-a-http-post-request-with-httpclient
@@ -80,7 +104,9 @@ public class MainActivity extends Activity {
             HttpResponse response = httpclient.execute(httppost);
             HttpEntity responseEntity = response.getEntity();
     		String result = EntityUtils.toString(responseEntity);
+    		
     		Log.i("a", result);
+    		jArr = new JSONArray(result);
             
         } catch (Exception e) {
             // TODO Auto-generated catch block
@@ -128,4 +154,73 @@ public class MainActivity extends Activity {
                 break;
         }
     }
+    
+    
+//    GridView
+    public static class GridView_Item
+    {
+    	public TextView txtTable, txtNumOfGuest;
+    }
+
+    public class myadapter extends BaseAdapter
+    {
+    	Context context;
+    	public myadapter(Context c)
+    	{
+    		context=c;
+    	}
+    	
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return jArr.length();
+		}
+
+		public Object getItem(int position) {
+			// TODO Auto-generated method stub
+			try {
+				return jArr.getJSONObject(position);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return position;
+		}
+
+		public long getItemId(int arg0) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		public View getView(int arg0, View arg1, ViewGroup arg2) {
+			// TODO Auto-generated method stub
+			GridView_Item gv_item;
+			LayoutInflater layoutinflater= ((Activity)context).getLayoutInflater();
+			
+			if(arg1==null)  
+			{  
+				gv_item = new GridView_Item();  
+				arg1 = layoutinflater.inflate(R.layout.gridview_item, null);  
+				gv_item.txtTable = (TextView) arg1.findViewById(R.id.txtTable);  
+				gv_item.txtNumOfGuest = (TextView) arg1.findViewById(R.id.txtNumOfGuest);  
+				arg1.setTag(gv_item);  
+			}  
+			else
+				gv_item = (GridView_Item)arg1.getTag();
+			
+			JSONObject jObj;
+			try {
+				jObj = jArr.getJSONObject(arg0);
+				gv_item.txtTable.setText("Bàn " + jObj.optString("id"));
+				gv_item.txtNumOfGuest.setText("Số khách hiện tại: " + jObj.optString("current_num_of_guest"));
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+			return arg1;
+		}
+    	
+    }
+
 }
